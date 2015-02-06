64fd2cf109382b812ef054a0ce8087419c599c3d
Christopher Treviño <christophertrev@gmail.com>
2015-02-06 15:44:04 -0800

    (feat) Serve up static files from client folder

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');
+
+
+var app = express();
+
+
+app.use(express.static(path.join(__dirname + '/../client')))
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+
+
 
 var server = app.listen(3000, function () {
 
