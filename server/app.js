1e96ad5ed432e1515114fddf4c99f17bca5731e2
Christopher Treviño <christophertrev@gmail.com>
2015-02-08 19:02:35 -0800

    (feat) add helper functions for passport

--- server/app.js
+++ server/app.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var path = require('path');
-var controllers = require('./controllers')
+var controllers = require('./controllers');
+var passport = require('passport');
+var session = require('express-session')
+var config = require('./config')
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+    // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, profile);
+    // });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 
 var app = express();
@@ -8,13 +34,11 @@ var app = express();
 
 app.use(express.static(path.join(__dirname + '/../client')));
 
-// app.get('/login',function (req, res){
+app.get('/auth/github',passport.authenticate('github'));
 
-// });
-
-// app.post('/login',function (req, res){
+app.post('/test',ensureAuthenticated,  function (req, res){
   
-// })
+})
 
 
 app.get('/', function (req, res) {
@@ -43,3 +67,10 @@ var server = app.listen(app.get('port'), function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 })
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
