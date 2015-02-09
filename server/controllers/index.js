87e052ea2129d0c0f7e681942bb7ea28db939a52
Christopher Treviño <christophertrev@gmail.com>
2015-02-08 16:40:25 -0800

    (feat) Add helper functions definitions f1or DB

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,20 @@
 var db = require('./../models');
+var controller = {};
 
 
+controller.addUser = function (firstName, lastName){
+
+}
+
+
+
+
+
+controller.addRating = function (rating, firstName, lastName){
+
+};
+
+
+
+
+module.exports = controller; 
